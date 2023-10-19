6b2dd38f3616bb865d99131c6dc06e23656608ee
Nikolay_Bondarenko <Nikolayb33@MacBook-Air-mac.local>
2023-10-19 10:40:52 +0300

    add answer at task 43

--- Program.cs
+++ Program.cs
@@ -5,25 +5,54 @@
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
 
-Console.WriteLine("Введите числа через пробел");
-string num = Console.ReadLine();
-string[] array = num.Split(" ");
+// Console.WriteLine("Введите числа через пробел");
+// string num = Console.ReadLine();
+// string[] array = num.Split(" ");
 
-int count = 0;
-for(int i = 0; i < array.Length; i++)
+// int count = 0;
+// for(int i = 0; i < array.Length; i++)
+// {
+//     if(Convert.ToInt32(array[i]) > 0)
+//     {
+// System.Console.WriteLine($"{Convert.ToInt32(array[i])} > 0");
+//         count ++;
+//     }
+//     else
+//     {
+//         continue;
+//     }
+// }
+// System.Console.WriteLine($"Положительных чисел в массиве - {count}");
+
+//Task 43 
+// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями 
+// y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+
+Console.WriteLine("Введите b1");
+int b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите k1");
+int k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите b2");
+int b2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите k2");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+if(k1 != k2)
 {
-    if(Convert.ToInt32(array[i]) > 0)
-    {
-        // System.Console.WriteLine($"{Convert.ToInt32(array[i])} > 0");
-        count ++;
-    }
-    else
-    {
-        continue;
-    }
+    double x = (b2 - b1) / (double)(k1 - k2);
+    double y = k1 * x + b1;
+    System.Console.WriteLine($"({x}; {y})");
 }
-System.Console.WriteLine($"Положительных чисел в массиве - {count}");
-
-
+else
+{
+    System.Console.WriteLine("Прямые параллельны");
+}
 
 
